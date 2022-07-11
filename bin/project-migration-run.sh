0dfd56c01f9bed6495bf3bb50f4560cb3e686a24
Loty Gero <lotgyero@gmail.com>
2022-07-11 22:40:28 +0300

    Try fix

--- bin/project-migration-run.sh
+++ bin/project-migration-run.sh
@@ -3,8 +3,8 @@
 WORK_PATH="`(dirname \"${0}\")`"
 WORK_PATH="`( cd \"${WORK_PATH}/../\" && pwd )`"
 
-# cd "${WORK_PATH}/migrations" && npm run debug:env;
-cd "${WORK_PATH}/migrations" && npm run debug:env ;
+cd "${WORK_PATH}/migrations" && npm run debug:env;
+# cd "${WORK_PATH}/migrations" && npm run migration:run ;
 EX_CODE=$?
 
 exit ${EX_CODE};
