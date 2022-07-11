4788b33c3e18f747c72ba634a24ca0984c7647d1
Loty Gero <lotgyero@gmail.com>
2022-07-11 19:45:27 +0300

    FiX port mistake

--- migrations/data-source.js
+++ migrations/data-source.js
@@ -8,7 +8,7 @@ import {DataSource, createConnection} from "typeorm";
 const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
