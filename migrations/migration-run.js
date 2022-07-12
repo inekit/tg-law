3e6fecb71880a516157d78c6946983ed5bda0f27
Loty Gero <lotgyero@gmail.com>
2022-07-12 07:37:43 +0300

    FiX migrate suxess

--- migrations/migration-run.js
+++ migrations/migration-run.js
@@ -1,42 +1,6 @@
 const process  = require('node:process');
 const console  = require('node:console');
 
-// const {
-//   DB_HOST,
-//   DB_PORT,
-//   DB_USER,
-//   DB_PASSWORD,
-//   DB_DATABASE,
-// } = process.env;
-
-// console.log({
-//   DB_HOST,
-//   DB_PORT,
-//   DB_USER,
-//   DB_PASSWORD,
-//   DB_DATABASE,
-// });
-
-// import {DataSource, createConnection} from "typeorm";
-
-// createConnection({
-//   type: "postgres",
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   username: DB_USER,
-//   password:  DB_PASSWORD,
-//   database: DB_DATABASE,
-//   logging: true,
-// })
-//   .then(async connection=>{
-//     console.log('connected');
-//     // console.log(connection);
-//   })
-//   .catch(error=>{
-//     console.error(error);
-//   });
-
-
 const { AppDataSource } = require('./data-source');
 
 AppDataSource
@@ -46,7 +10,7 @@ AppDataSource
     AppDataSource
       .runMigrations()
       .then((data)=>{
-        console.lg('runMigrations data', data);
+        console.log('runMigrations data', data);
       })
       .catch(error=>{
         console.log('env-test error:',error);
@@ -55,16 +19,3 @@ AppDataSource
   }).catch((err)=>{
     console.error('error init', err);
   });
-
-
-// AppDataSource
-//   .connection()
-//   .runMigrations()
-//   .then((data)=>{
-//     console.lg('runMigrations data', data);
-//   })
-//   .catch(error=>{
-//     console.log('env-test error:',error);
-//   });
-
-//process.exit(0);
