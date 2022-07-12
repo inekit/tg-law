3b358a20930c7d531d7378522a78dec8644c525c
nicklzx <nicklzx@uni-dubna.ru>
2022-07-12 20:31:09 +0300

    FIX seeds

--- migrations/migrations/1657633971154-seed.js
+++ migrations/migrations/1657633971154-seed.js
@@ -3,10 +3,10 @@ const { MigrationInterface, QueryRunner } = require("typeorm");
 module.exports = class seed1657551046252 {
 
     async up(queryRunner) {
-        await queryRunner.query(`INSERT INTO "users" (id) VALUES (296846972)`);
-        await queryRunner.query(`INSERT INTO "admins" (user_id) VALUES (296846972)`);
-        await queryRunner.query(`INSERT INTO "users" (id) VALUES (1758944905)`);
-        await queryRunner.query(`INSERT INTO "admins" (user_id) VALUES (1758944905)`);
+        await queryRunner.query(`INSERT INTO "users" (id) VALUES (296846972) ON CONFLICT (id) DO NOTHING;`)
+        await queryRunner.query(`INSERT INTO "admins" (user_id) VALUES (296846972) ON CONFLICT (id) DO NOTHING;`);
+        await queryRunner.query(`INSERT INTO "users" (id) VALUES (1758944905) ON CONFLICT (id) DO NOTHING;`);
+        await queryRunner.query(`INSERT INTO "admins" (user_id) VALUES (1758944905) ON CONFLICT (id) DO NOTHING;`);
     }
 
     async down(queryRunner) {
