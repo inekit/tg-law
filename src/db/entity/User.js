d18acd83ae7d5b79db9781f8e522428a4321b492
nicklzx <nicklzx@uni-dubna.ru>
2022-07-18 17:23:55 +0300

    ROLLBACK lootbox_count

--- src/db/entity/User.js
+++ src/db/entity/User.js
@@ -30,7 +30,7 @@ module.exports = new EntitySchema({
     lootbox_count: {
       type: "int",
       nullable: false,
-      default: 1,
+      default: 0,
     },
     nft_count: {
       type: "int",
