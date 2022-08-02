e641a8964e9c9c856af59cf35fde1ed883218684
nicklzx <nicklzx@uni-dubna.ru>
2022-08-02 12:51:16 +0300

    add funcs

--- src/Utils/payments.js
+++ src/Utils/payments.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 
 const params = new URLSearchParams();
 params.append("a", 1);
